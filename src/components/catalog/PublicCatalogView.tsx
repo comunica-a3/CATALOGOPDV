7e3f357fe327a2960b7148945afead30ef3edaf7
comunica-a3 <comunica.a3@gmail.com>
2026-09-18 15:08:12 -0300

    Update PublicCatalogView.tsx

--- src/components/catalog/PublicCatalogView.tsx
+++ src/components/catalog/PublicCatalogView.tsx
@@ -186,7 +186,13 @@ export const PublicCatalogView: React.FC<PublicCatalogViewProps> = ({
     const shouldFetch = isStandalone || !propItems || propItems.length === 0;
     if (shouldFetch) {
       setIsLoadingStatic(true);
-      fetch(`/catalogo.json?t=${Date.now()}`)
+      const urlCatalogo = `https://raw.githubusercontent.com/comunica-a3/CATALOGOPDV/main/public/catalogo.json?t=${Date.now()}`;
+      const res = await fetch(urlCatalogo);
+if (res.ok) {
+  const data = await res.json();
+  setProducts(data.products || []);
+  setCategories(data.categories || []);
+}
         .then((res) => {
           if (!res.ok) throw new Error(`HTTP ${res.status}`);
           return res.json();
